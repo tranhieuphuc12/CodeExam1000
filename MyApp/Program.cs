e682335079a2506bff37cd19b38f1dddd5b11b83
tranhieuphuc12 <116535717+tranhieuphuc12@users.noreply.github.com>
2023-02-19 10:24:45 +0700

    Add files via upload

---
+++ MyApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace MyApp{
+    public static class Program{
+
+        public static void Main(String[] args){
+
+           //declare variable
+           int num = 0;
+           int flag =0;
+           int max = 0;
+           //input
+           do{
+            System.Console.WriteLine("Enter a number: ");
+           int.TryParse(Console.ReadLine(), out num);
+
+           }while(num <= 0);
+
+           //processing
+           do
+           {
+            
+           }while();
+
+        }
+    }
+}
